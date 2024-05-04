fb7f45be8b630b155dfd013f49d9b47967ab78f2
zjianru <864156480@qq.com>
2024-05-04 11:09:00 +0800

    init project

--- src/main/java/com/cz/registry/CzRegistryApplication.java
+++ src/main/java/com/cz/registry/CzRegistryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CzRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CzRegistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CzRegistryApplication.class, args);
+    }
 
 }
--- src/test/java/com/cz/registry/CzRegistryApplicationTests.java
+++ src/test/java/com/cz/registry/CzRegistryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CzRegistryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
