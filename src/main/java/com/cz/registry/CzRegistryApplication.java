4e7ad15b1f1b624bc54b409e32cea44f4a0d6d5f
zjianru <864156480@qq.com>
2024-05-04 11:08:26 +0800

    init project

---
+++ src/main/java/com/cz/registry/CzRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.cz.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cz/registry/CzRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
